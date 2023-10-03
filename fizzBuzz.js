0b4fc23c7f9f151969c4f1febad9065a34a4e9a3
javiercastaneda303 <javiercastaneda303@gmail.com>
2023-10-03 12:54:51 +0200

    ahora todos  pasan

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,17 @@
 function fizzBuzz(n) {
+    let res = ""
+    console.log(`introduciendo un ${n} nos da: ${n % 3}`)
+    if (n === 0) {
+        return "0";
+    }
     if (n % 3 == 0) {
-        return "fizz";
+        res += "fizz";
+    }
+    if (n % 5 == 0) {
+        res += "buzz";
+    }
+    if (res != "") {
+        return res;
     }
     return n.toString();
 }
