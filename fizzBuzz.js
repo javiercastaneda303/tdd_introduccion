d1d6c13a279e9b81e1a47ec393bfb857c06870e8
javiercastaneda303 <javiercastaneda303@gmail.com>
2023-10-04 10:49:47 +0200

    Conseguido!!!    throw new Error("n no es un numero")

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,14 +1,24 @@
 function fizzBuzz(n) {
     let res = ""
-    console.log(`introduciendo un ${n} nos da: ${n % 3}`)
+    try {
+        if ("number" !== typeof n) {
+            throw new Error("n no es un numero")
+            // console.log(`valor incorrecto \n n: ${n} \n typeof n: ${typeof n}`)
+            // return ""
+        }
+    } catch (err) {
+        // throw new Error("n no es un numero")
+        // throw new Error(`Ups: no entiendo el valor que me estás pasando \n err: ${err}`)
+        console.log(`Ups: no entiendo el valor que me estás pasando \n err: ${err}`)
+    }
     if (n === 0) {
         return "0";
     }
     if (n % 3 == 0) {
-        res += "fizz";
+        res += "Fizz";
     }
     if (n % 5 == 0) {
-        res += "buzz";
+        res += "Buzz";
     }
     if (res != "") {
         return res;
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,38 +1,67 @@
 const fizzBuzz = require('./fizzBuzz');
 
-test('1-a  return fizz  3', () => {
-    expect(fizzBuzz(3)).toBe("fizz");
+test('1-  return Fizz  3', () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
 });
 
-test('1-b  return fizz  6', () => {
-    expect(fizzBuzz(6)).toBe("fizz");
+test('1-  return Fizz  6', () => {
+    expect(fizzBuzz(6)).toBe("Fizz");
 });
 
-test('1-c  return fizz  9', () => {
-    expect(fizzBuzz(9)).toBe("fizz");
-});
-
-test('1-d  return fizz  12', () => {
-    expect(fizzBuzz(12)).toBe("fizz");
+test('1-  return Fizz  9', () => {
+    expect(fizzBuzz(9)).toBe("Fizz");
 });
 
 
-test('1-e  return != fizz  15', () => {
-    expect(fizzBuzz(15)).not.toBe("fizz");
+test('1-  return string  0', () => {
+    expect(fizzBuzz(0)).toBe("0");
 });
 
-test('1-f  return != fizz  10', () => {
-    expect(fizzBuzz(10)).not.toBe("fizz");
+test('1-  return string  1', () => {
+    expect(fizzBuzz(1)).toBe("1");
 });
 
-test('1-g  return != fizz  1', () => {
-    expect(fizzBuzz(1)).not.toBe("fizz");
+test('1-  return string  11', () => {
+    expect(fizzBuzz(11)).toBe("11");
+});
+test('1-  return Fizz  12', () => {
+    expect(fizzBuzz(12)).toBe("Fizz");
 });
 
-test('1-h  return != fizz  11', () => {
-    expect(fizzBuzz(11)).not.toBe("fizz");
+test('1-  return != Fizz  15', () => {
+    expect(fizzBuzz(15)).not.toBe("Fizz");
 });
 
-test('1-i  return != fizz  0', () => {
-    expect(fizzBuzz(0)).not.toBe("fizz");
+
+test('2-  return  Buzz  5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
 });
+
+
+test('2-  return  Buzz  10', () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+});
+
+test('2-  return  FizzBuzz  15', () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+});
+
+// if !== number
+test('3- ¿y si le paso un * ?', () => {
+    // expect(fizzBuzz("*")).toThrow();
+    // expect(fizzBuzz("*")).toThrowError("n no es un numero");
+    try {
+        expect(fizzBuzz("*"))
+    } catch (err) {
+        expect(err).toBe("n no es un numero");
+        //expect(err).toBeInstanceOf(Error);
+        //expect(err).toHaveProperty("n no es un numero"); // https://youtu.be/6NPaTSj7Gtk?si=3789Je0r24SoFPof&t=328
+    }
+});
+
+// el float no lo tengo contemplado
+
+
+// test(' * => ???', () => {
+//     expect(fizzBuzz(`*`)).toBe("string");
+// });
